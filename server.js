26a8f1b1a055fab867c98a268f69e280a171b193
Matt DSena <matthewdsena@gmail.com>
2022-03-07 16:57:25 +1030

    main menu prompts

--- server.js
+++ server.js
@@ -9,3 +9,53 @@ const connection = mysql.createConnection({
     password: "root",
     database: "employees_db"
   });
+
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log("--- EMPLOYEES ---")
+    mainMenu();
+  });
+  
+  //main menu prompts
+  const mainMenu = () => {
+    inquirer.prompt ([
+      {
+        type: 'list',
+        name: 'mainmenu', 
+        message: 'What would you like to do?',
+        choices: ['View All Departments',
+                  'View All Roles',
+                  'View All Employees', 
+                  'Add Department',
+                  'Add Role',
+                  'Add Employee',
+                  'Update Employee Role'             
+                  ]
+      }
+    ]).then((answer) => {
+      switch (answer.mainmenu) {
+        case 'View All Departments':
+          viewAllDepartments();
+          break;
+        case 'View All Roles':
+          viewAllRoles();
+          break;
+        case 'View All Employees':
+          viewAllEmployees();
+          break; 
+        case 'Add Department':
+          addDepartment();
+          break;
+        case 'Add Role':
+          addRole();
+          break;
+        case 'Add Employee':
+          addEmployee();
+          break;
+        case 'Update Employee Role':
+          updateEmployeesRole();
+          break;
+  
+      }
+    });
+  }
