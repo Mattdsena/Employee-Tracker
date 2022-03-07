c7635e76cb55352128360117225bb255c70b0eeb
Matt DSena <matthewdsena@gmail.com>
2022-03-07 15:01:32 +1030

    create connection to mysql database

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const consoleTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "employees_db"
+  });
