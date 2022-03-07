8b299c7e66079fc7afa77367bcbf89907e359362
Matt DSena <matthewdsena@gmail.com>
2022-03-07 18:32:03 +1030

    display a table of all employees

--- server.js
+++ server.js
@@ -75,3 +75,11 @@ function viewAllRoles () {
         mainMenu();
     });
   }
+
+  function viewAllEmployees() {
+    connection.query("SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS Manager FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON d.id = r.department_id LEFT JOIN employee m ON m.id = e.manager_id", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        mainMenu();
+    });
+  }
